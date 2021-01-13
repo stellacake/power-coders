653127a98dbe5a23ac0733d89c39fe4505fdedf2
FlorianPx <florian.poux@hotmail.fr>
2021-01-14 00:26:06 +0100

    feat(games): Create white russian game

--- power-coders/src/App.js
+++ power-coders/src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Moscow from "./components/Rooms/Moscow/Moscow";
-import Newspaper from "./components/Rooms/Moscow/games/Newspaper";
+import Newspaper from "./components/Rooms/Moscow/games/newspaper/Newspaper";
+import WhiteRussian from "./components/Rooms/Moscow/games/whiteRussian/WhiteRussian";
+
 import "./App.css";
 
 function App() {
-	return (
-		<div className="App">
-			<Moscow />
-      <Newspaper />
-		</div>
-	);
+  return (
+    <div className="App">
+      {/* <Moscow />
+      <Newspaper /> */}
+      <WhiteRussian />
+    </div>
+  );
 }
 
 export default App;
---
+++ power-coders/src/components/Rooms/Moscow/games/whiteRussian/whiteRussianConstants.js
@@ -0,0 +1,44 @@
+export const whiteRussianArray = [
+  {
+    name: "vodka",
+    value: true,
+  },
+  {
+    name: "liqueur de café",
+    value: true,
+  },
+  {
+    name: "lait",
+    value: true,
+  },
+  {
+    name: "crème fraîche (liquide ou épaisse)",
+    value: true,
+  },
+  {
+    name: "glaçons",
+    value: true,
+  },
+  {
+    name: "vin de liqueur",
+    value: false,
+  },
+  {
+    name: "crème entière",
+    value: false,
+  },
+  {
+    name: "Kvas",
+    value: false,
+  },
+  {
+    name: "jaune d'œuf",
+    value: false,
+  },
+  {
+    name: "citron vert",
+    value: false,
+  },
+];
+
+export const factisArray = [];
