4a8d6ff0e6a2b43bbeeb194ee8c582f2c86506d9
J <jonathan.arnould33@gmail.com>
2021-01-14 17:13:18 +0100

    new fact

--- power-coders/src/components/Rooms/Moscow/culturalFacts.js
+++ power-coders/src/components/Rooms/Moscow/culturalFacts.js
@@ -1,24 +1,24 @@
 export const moscowFacts = {
-	book:
-		"Tolstoï a lu d’une traite tout Kant et tout Schopenhauer. Après avoir terminé d’écrire Guerre et Paix, notre écrivain, qui venait d’y consacrer six années entières, se trouva quelque peu désœuvré. Il lut alors l’intégralité de ces deux philosophes allemands et, la pensée du second le séduisant particulièrement, il se mit en tête de le traduire en russe. Finalement, devant l’ampleur de la tâche, il se contenta d’en accrocher un portrait dans son bureau.",
-	window:
-		"Le nom de la Place Rouge ne vient ni de la couleur des briques rouges environnantes, ni du lien entre cette couleur et le communisme. Une traduction plus exacte de son nom russe serait la «Belle Place». En russe ancien, krasny signifie à la fois rouge et beau et doit ici être compris dans ce dernier sens, aujourd'hui archaïque (beau devient krassivy en russe moderne).",
-	painting:
-		"Vassily Kandinsky a été un des pionniers de l’art abstrait. L’idée lui serait venue un soir, alors qu’il rentrait chez lui. En poussant la porte, il aperçut un magnifique tableau «d’une indescriptible beauté, baigné de couleurs intérieures». Il n’y retrouva alors rien d’autre que des formes et des couleurs, n’y reconnut aucun sujet, puis il se rendit soudain compte qu’il s’agissait d’une de ses toiles renversée sur le côté, contre le mur.",
-	mir:
-		"La station Mir, du russe Мир signifie Paix et Monde. L'astronaute Sergueï Krikalev a assisté depuis Mir à la disparition de son pays, l'URSS, où Boris Eltsine succédait à Mikhaïl Gorbatchev. Parti d'URSS, revenu en Russie.",
-	musicbox:
-		"Anna Pavlova est considérée pour beaucoup comme la meilleure danseuse de ballet classique. C'est elle qui a donné son nom à la célèbre pâtisserie. Elle a été une étoile du Ballet impérial russe, des Ballets russes de Serge Diaghilev et la première ballerine à parcourir le monde avec sa propre compagnie.",
-	phone:
-		"Non, ce ne sont pas les Etats-Unis au bout du fil ! Mais pourquoi ne pas appeler vos proches pour prendre de leurs nouvelles ? ;) ",
-	chapka:
-		"Il fait bien trop beau dehors pour sortir la chapka ! D'ailleurs, ce mot signifie bonnet. Son vrai nom c'est ouchanka. Sa particularité est d'avoir deux parties rabattables qui peuvent couvrir les oreilles et la nuque, ou bien être relevées et nouées sur la coiffe.",
-	tea:
-		"La Russie impériale découvre le thé en 1638, lorsqu’un ambassadeur russe en rapporté au tsar, offert par un prince mongol. Le thé dit «à la russe» est préparé dans un samovar, au sommet duquel on fait infuser du thé noir de Chine en grande quantité. Le goût est un peu amer et c'est pourquoi les Russes accompagnent leur thé de miel, citron, bergamote et autres agrumes pour neutraliser les tanins.",
-	bin:
-		"Un pass de terminé ! En même temps, le métro de Moscou est le plus fréquenté et le plus étendu d’Europe. Il y a plus de 200 stations, dont beaucoup s’apparentent à de véritables œuvres d’art. Grâce à sa démesure et à sa riche histoire, le réseau fait en outre l’objet de nombreuses légendes urbaines.",
-	bottle:
-		"Comme si on ne vous avait pas vus venir... Cela a beau être une spécialité, l'abus d'alcool est dangereux pour la santé ! A consommer avec modération ;)",
+  book:
+    "Tolstoï a lu d’une traite tout Kant et tout Schopenhauer. Après avoir terminé d’écrire Guerre et Paix, notre écrivain, qui venait d’y consacrer six années entières, se trouva quelque peu désœuvré. Il lut alors l’intégralité de ces deux philosophes allemands et, la pensée du second le séduisant particulièrement, il se mit en tête de le traduire en russe. Finalement, devant l’ampleur de la tâche, il se contenta d’en accrocher un portrait dans son bureau.",
+  window:
+    "Le nom de la Place Rouge ne vient ni de la couleur des briques rouges environnantes, ni du lien entre cette couleur et le communisme. Une traduction plus exacte de son nom russe serait la «Belle Place». En russe ancien, krasny signifie à la fois rouge et beau et doit ici être compris dans ce dernier sens, aujourd'hui archaïque (beau devient krassivy en russe moderne).",
+  painting:
+    "Vassily Kandinsky a été un des pionniers de l’art abstrait. L’idée lui serait venue un soir, alors qu’il rentrait chez lui. En poussant la porte, il aperçut un magnifique tableau «d’une indescriptible beauté, baigné de couleurs intérieures». Il n’y retrouva alors rien d’autre que des formes et des couleurs, n’y reconnut aucun sujet, puis il se rendit soudain compte qu’il s’agissait d’une de ses toiles renversée sur le côté, contre le mur.",
+  mir:
+    "La station Mir, du russe Мир signifie Paix et Monde. L'astronaute Sergueï Krikalev a assisté depuis Mir à la disparition de son pays, l'URSS, où Boris Eltsine succédait à Mikhaïl Gorbatchev. Parti d'URSS, revenu en Russie.",
+  musicbox:
+    "Anna Pavlova est considérée pour beaucoup comme la meilleure danseuse de ballet classique. C'est elle qui a donné son nom à la célèbre pâtisserie. Elle a été une étoile du Ballet impérial russe, des Ballets russes de Serge Diaghilev et la première ballerine à parcourir le monde avec sa propre compagnie.",
+  phone:
+    "Non, ce ne sont pas les Etats-Unis au bout du fil ! Mais pourquoi ne pas appeler vos proches pour prendre de leurs nouvelles ? ;) ",
+  chapka:
+    "Il fait bien trop beau dehors pour sortir la chapka ! D'ailleurs, ce mot signifie bonnet. Son vrai nom c'est ouchanka. Sa particularité est d'avoir deux parties rabattables qui peuvent couvrir les oreilles et la nuque, ou bien être relevées et nouées sur la coiffe.",
+  tea:
+    "La Russie impériale découvre le thé en 1638, lorsqu’un ambassadeur russe en rapporté au tsar, offert par un prince mongol. Le thé dit «à la russe» est préparé dans un samovar, au sommet duquel on fait infuser du thé noir de Chine en grande quantité. Le goût est un peu amer et c'est pourquoi les Russes accompagnent leur thé de miel, citron, bergamote et autres agrumes pour neutraliser les tanins.",
+  bin:
+    "Un pass de terminé ! En même temps, le métro de Moscou est le plus fréquenté et le plus étendu d’Europe. Il y a plus de 200 stations, dont beaucoup s’apparentent à de véritables œuvres d’art. Grâce à sa démesure et à sa riche histoire, le réseau fait en outre l’objet de nombreuses légendes urbaines.",
+  bottle:
+    "Comme si on ne vous avait pas vu venir... Cela a beau être une spécialité, l'abus d'alcool est dangereux pour la santé ! A consommer avec modération ;) et Nasdrovia!",
 };
 
 export const otherFacts = "";
