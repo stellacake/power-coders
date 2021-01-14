e3019b2a59ec46912a29932ff40c551f77b74bda
surlejonathan <surle.jonathan@gmail.com>
2021-01-14 12:49:18 +0100

    finally did it with socket.io

--- power-coders/src/App.js
+++ power-coders/src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import { Join, Chat } from "./components/LoginPage";
 import Moscow from "./components/Rooms/Moscow/Moscow";
 import Newspaper from "./components/Rooms/Moscow/games/Newspaper";
 import "./App.css";
 
 function App() {
-	return (
-		<div className="App">
-			<Moscow />
-      <Newspaper />
-		</div>
-	);
+  return (
+    <Router>
+      <Route path='/' exact component={Join} />
+      <Route path='/chat' component={Chat} />
+      {/* 	<Moscow />
+      <Newspaper /> */}
+    </Router>
+  );
 }
 
 export default App;
